713e4ac32e9f526c4675ddb4dc4eee66d6a8b040
baiguomin <623687677@qq.com>
2017-05-06 21:38:27 +0800

    add the comments of the steps about how to build this framework

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # neruda
 ioc aop dao mvc framework
+
+1.模仿spring的ioc功能
+第一步，读取配置文件，主要代码在BaseProperty.java类中
+  目前读取配置文件在类加载阶段，后期应该是做到当项目启动时才读取配置文件（√）
+第二步，在application.properties中配置基础包
+新建@Bean 注解类，然后扫描项目中被@Bean注解的类，用一个HashMap管理这些类（ing）
--- src/main/java/cn/neruda/base/util/BaseProperty.java
+++ src/main/java/cn/neruda/base/util/BaseProperty.java
@@ -10,13 +10,14 @@ import java.util.Set;
  * Created by baiguomin on 2017/5/4.
  */
 public class BaseProperty {
+    public static final String baseProperties = "application.properties";
     public static Map<String,Object> configMap = new HashMap<>();
 
     static {
         Properties prop = new Properties();
         try {
             InputStream in =
-                    ClassLoader.getSystemResourceAsStream("application.properties");
+                    ClassLoader.getSystemResourceAsStream(baseProperties);//
             prop.load(in);
             in.close();
             Set<Object> objects = prop.keySet();
